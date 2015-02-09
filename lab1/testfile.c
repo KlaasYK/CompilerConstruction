4ad8650a5d119db5e7a3f7fb1cba2639b04cd843
CC007 <Coolcat_the_best2@hotmail.com>
2015-02-09 19:45:43 +0100

    created operatorHelper

--- lab1/testfile.c
+++ lab1/testfile.c
@@ -2,27 +2,104 @@
 #include <assert.h>
 #include "bigints.c"
 
-int main() {
+void operatorHelper(const char *aStr, const char *bStr, void (*f)(Integer *, Integer), char operator){
+	
 	Integer a, b;
-	makeIntegerFromString(&a, "-50545936");
+	makeIntegerFromString(&a, aStr);
+	makeIntegerFromString(&b, bStr);
+	printInteger(a);
+	printf(" %c ", operator);
+	printInteger(b);
+	printf(" = ");
+	(*f)(&a, b);
 	printInteger(a);
 	printf("\n");
 	freeInteger(&a);
+	freeInteger(&b);
+	
+}
+
+void testPrintInteger(){
+	
+}
+
+void testMakeIntegerFromString(){
+	Integer a;
+	makeIntegerFromString(&a, "-50545000936034002");
+	printInteger(a);
+	printf("\n");
+	freeInteger(&a);
+}
+
+void testAddInteger(){
+	Integer a, b;
+	
+	operatorHelper("2", "2", addInteger, '+');
+	operatorHelper("293487239847239847928347", "237957909275092850928355", addInteger, '+');
+	operatorHelper("25435", "1", addInteger, '+');
+	operatorHelper("1", "25435", addInteger, '+');
+	operatorHelper("1", "999", addInteger, '+');
+	operatorHelper("999", "1", addInteger, '+');
+	operatorHelper("500", "501", addInteger, '+');
+	operatorHelper("501", "500", addInteger, '+');
 /*
+	
+	makeIntegerFromString(&a, "293487239847239847928347");
+	makeIntegerFromString(&b, "237957909275092850928355");
+	printInteger(a);
+	printf("+");
+	printInteger(b);
+	printf("=");
+	addInteger(&a, b);
+	printInteger(a);
+	printf("\n");
+	freeInteger(&a);
+	freeInteger(&b);
+	
+	makeIntegerFromString(&a, "283049");
+	makeIntegerFromString(&b, "1");
+	printInteger(a);
+	printf("+");
+	printInteger(b);
+	printf("=");
+	addInteger(&a, b);
+	printInteger(a);
+	printf("\n");
+	freeInteger(&a);
+	freeInteger(&b);
+	
+	makeIntegerFromString(&a, "283049");
+	makeIntegerFromString(&b, "1");
+	printInteger(a);
+	printf("+");
+	printInteger(b);
+	printf("=");
+	addInteger(&a, b);
+	printInteger(a);
+	printf("\n");
+	freeInteger(&a);
+	freeInteger(&b);
+*/
+	
+}
+
+void testSubInteger(){
+	Integer a, b;
 	makeIntegerFromString(&a, "-98");
 	makeIntegerFromString(&b, "-998");
 	printInteger(a);
-	printf("\n");
+	printf("-");
 	printInteger(b);
-	printf("\n");
+	printf("=");
 
 	subInteger(&a, b);
 	printInteger(a);
 	printf("\n");
+	
+}
 
-	freeInteger(&a);
-	freeInteger(&b);
-
+void testMulInteger(){
+	Integer a, b;
 
 	makeIntegerFromString(&a, "23663");
 	makeIntegerFromString(&b, "2356");
@@ -36,6 +113,21 @@ int main() {
 
 	freeInteger(&a);
 	freeInteger(&b);
-*/
+}
+
+void testDivInteger(){
+	
+}
+
+void testModInteger(){
+	
+}
+
+void testPowInteger(){
+	
+}
+
+int main() {
+	testAddInteger();
 	return 0;
 }
