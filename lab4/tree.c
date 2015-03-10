4a03a5ddc56cc93939060e5d7611742a78bd8ebd
CC007 <Coolcat_the_best2@hotmail.com>
2015-03-10 10:00:59 +0100

    started tree.c

---
+++ lab4/tree.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+
+enum UnOp {
+    not, min
+}
+
+enum BinOp {
+    plus, min, mul, div, mod, pow, and, or, cand, cor
+}
+
+typedef struct Expression {
+} *Exp;
+
+typedef struct UnNode {
+    Exp e;
+    UnOp operator;
+} *Unode;
+
+typedef struct BinNode {
+    Exp l, r;
+    Binop operator;
+} *Bnode;
+
+Bnode makeBinNode(Exp l, Exp r, BinOp op) {
+    Bnode bin = malloc(sizeof (Bnode));
+    bin->l = l;
+    bin->r = r;
+    bin->operator = op;
+    return bin;
+}
+
+Unode makeUnNode(Exp e, UnOp op) {
+    Unode un = malloc(sizeof (Unode));
+    un->e = e;
+    un->operator = op;
+    return un;
+}
+
+void freeExp(Exp e);
+
+void freeBinNode(Bnode bin){
+    freeExp(bin->l);
+    freeExp(bin->r);
+    free(bin);
+}
+
+void freeUnNode(Unode un){
+    freeExp(un->e);
+    free(un);
+}
