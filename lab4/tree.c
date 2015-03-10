075451e776fb2415c6051e3c5057e24788708670
CC007 <Coolcat_the_best2@hotmail.com>
2015-03-10 19:46:23 +0100

    some more stuff, I forgot what

--- lab4/tree.c
+++ lab4/tree.c
@@ -1,10 +1,101 @@
 #include <stdlib.h>
 #include "symboltable.h"
 #include "tree.h"
+enum Boolval {
+    true, false
+}
 
+enum UnOp {
+    notop, minop
+}
+
+enum BinOp {
+    plusop, minop, mulop, divop, modop, powop, andop, orop, candop, corop
+}
+
+enum ExpKind {
+    unodeexp, bnodeexp, idexp, funcexp, intexp, boolexp
+}
+
+typedef identifier *ID;
+
+typedef functioncall *FuncCall;
+
+typedef integer *Int;
+
+typedef boolean *Bool;
+
+typedef Expression *Exp;
+
+typedef UnNode *Unode;
+
+typedef BinNode *Bnode;
+
+struct identifier {
+    int type;
+    char* name;
+};
+
+struct functioncall {
+    ID id;
+    Exp *params;
+};
+
+struct integer {
+    char* value;
+};
+
+struct boolean {
+    Boolval value;
+};
+
+struct Expression {
+    ExpKind kind;
+
+    union {
+        Unode unode;
+        Bnode bnode;
+        ID id;
+        FuncCall funcCall;
+        Int intval;
+        Bool boolval;
+    } node;
+};
+
+struct UnNode {
+    Exp e;
+    UnOp operator;
+};
+
+struct BinNode {
+    Exp l, r;
+    Binop operator;
+};
+
+ID makeID(int type, char* name){
+    ID id = malloc(sizeof(identifier));
+    
+}
+
+
+Exp makeUnNodeExp(Unode un){
+    Exp exp = malloc(sizeof(expression));
+    exp->kind = unodeexp;
+    exp->node.unode = un;
+}
+Exp makeBinNodeExp(Bnode bin){
+    Exp exp = malloc(sizeof(expression));
+    exp->kind = bnodeexp;
+    exp->node.bnode = bin;
+}
+Exp makeIDNodeExp(Bnode bin){
+    Exp exp = malloc(sizeof(expression));
+    exp->kind = idexp;
+    exp->node.bnode = bin;
+}
 
 Bnode makeBinNode(Exp l, Exp r, BinOp op) {
-    Bnode bin = malloc(sizeof (Bnode));
+    Bnode bin = malloc(sizeof (BinNode));
     bin->l = l;
     bin->r = r;
     bin->operator = op;
@@ -12,11 +103,13 @@ Bnode makeBinNode(Exp l, Exp r, BinOp op) {
 }
 
 Unode makeUnNode(Exp e, UnOp op) {
-    Unode un = malloc(sizeof (Unode));
+    Unode un = malloc(sizeof (UnNode));
     un->e = e;
     un->operator = op;
     return un;
 }
+
+Exp makeBinNodeExp(Bnode bin)
 
 void freeExp(Exp e);
 
