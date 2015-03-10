b4aa7338afa37dd6f112a7474e3233ae3ad5e5a3
CC007 <Coolcat_the_best2@hotmail.com>
2015-03-10 10:35:34 +0100

    used tree.h

--- lab4/tree.c
+++ lab4/tree.c
@@ -1,62 +1,7 @@
 #include <stdlib.h>
 #include "symboltable.h"
 
-enum Boolval {
-    true, false
-}
 
-enum UnOp {
-    not, min
-}
-
-enum BinOp {
-    plus, min, mul, div, mod, pow, and, or, cand, cor
-}
-
-enum ExpKind {
-    unodeexp, bnodeexp, idexp, funcexp, intexp, boolexp
-}
-
-typedef struct identifier {
-    int type;
-    char* name;
-} *ID;
-
-typedef struct functioncall {
-    ID id;
-    Exp *params;
-} *FuncCall;
-
-typedef struct integer {
-    int value;
-} *Int;
-
-typedef struct boolean {
-    Boolval value;
-} *Bool;
-
-typedef struct Expression {
-    ExpKind kind;
-
-    union {
-	Unode unode;
-	Bnode bnode;
-	ID id;
-	FuncCall funcCall;
-	Int intval;
-	Bool boolval;
-    };
-} *Exp;
-
-typedef struct UnNode {
-    Exp e;
-    UnOp operator;
-} *Unode;
-
-typedef struct BinNode {
-    Exp l, r;
-    Binop operator;
-} *Bnode;
 
 Bnode makeBinNode(Exp l, Exp r, BinOp op) {
     Bnode bin = malloc(sizeof (Bnode));
