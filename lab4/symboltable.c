72ee5426b6cfb2f5cca03b03f07deba7455ba5bf
Klaas Kliffen <klaaskliffen@msn.com>
2015-03-12 14:25:10 +0100

    Extended the symboltable code

--- lab4/symboltable.c
+++ lab4/symboltable.c
@@ -24,22 +24,110 @@ void freeNode(Node *nodeptr) {
 }
 
 void initSymbolTable() {
-	List top =  malloc(sizeof(List));
-	if (top == NULL) {
+	symboltable = malloc(sizeof(Stack));
+	List *top =  malloc(sizeof(List));
+	if (top == NULL || symboltable == NULL) {
 		printf("Allocation error!\n");
 		exit(EXIT_FAILURE);
 	}
 	/* make the first (global) scope empty */
 	top->first = NULL;
 	top->nextlist = NULL;
+	symboltable->top = top;
+}
+
+void freeList(List *list) {
+	// free all list items
+	while (list->first != NULL) {
+		Node *n = (top->first)->next;
+		// free the parameters list (if present)
+		Node *param = (top->first)->parameters
+		while (param != NULL) {
+			Node *nextparam = param->next;
+			free(param);
+			param = nextparam;
+		}
+		free(list->first);
+		list->first = n;
+	}
+	// free the list itself
+	free(list);
 }
 
 void freeSymbolTable() {
-	
+	if (symboltable == NULL) {
+		printf("Symboltable not initialised!\n");
+		exit(EXIT_FAILURE);
+	}
+	while (symboltable->top != NULL) {
+		List *top = symboltable->top;
+		List nextlist = top->nextlist;
+		freeList(top);
+		symboltable->top = nextlist;
+	}
+	// free the symboltable
+	free(symboltable);
+}
+
+void putBlock() {
+	if (symboltable == NULL) {
+		printf("Symboltable not initialised!\n");
+		exit(EXIT_FAILURE);
+	}
+	List *newtop = malloc(sizeof(List));
+	if (newtop == NULL) {
+		printf("Allocation error!\n");
+		exit(EXIT_FAILURE);
+	}
+	newtop->first = NULL;
+	newtop->nextlist = symboltable->top;
+	symboltable->top = newtop;
+}
+
+void popBlock() {
+	if (symboltable == NULL) {
+		printf("Symboltable not initialised!\n");
+		exit(EXIT_FAILURE);
+	}
+	if (symboltable->top == NULL) {
+		printf("Symboltable empty!\n");
+		exit(EXIT_FAILURE);
+	}
+	List *oldtop = symboltable->top;
+	symboltable->top = oldtop->nextlist;
+	freeList(oldtop);
+	free(oldtop);
 }
 
 void insertIdentifier(char *name, NodeType ntype, int etype, Node *paramlist) {
-	
+	Node *n = malloc(sizeof(Node));
+	if (n == NULL) {
+		printf("Allocation eror!\n");
+		exit(EXIT_FAILURE);
+	}
+	n->name = name;
+	n->type = ntype;
+	n->evaltype = etype;
+	n->parameters = paramlist;
+	n->next = (symboltable->top)->first;
+	(symboltable->top)->first = n;
 }
 
+/* return the node with the given name */
+Node* getNode(char *name) {
+	Node *n = NULL;
+	List *c = symboltable->top;
+}
+
+NodeType lookupType(char *name) {
+	return 0;
+}
+
+int lookupEvalType(char *name, NodeType ntype) {
+	return 0;
+}
+
+Node* lookupParams(char *name) {
+	return NULL;
+}
 /* EOF symboltable.c */
--- lab4/symboltable.h
+++ lab4/symboltable.h
@@ -30,9 +30,23 @@ typedef struct Stack {
 	List *top;
 }
 
-Stack symboltable;
+Stack *symboltable = NULL;
 
+/* initialise the symbol table */
 void initSymbolTable();
+/* frees the memory of the symboltable*/
+void freeSymbolTable();
+/* puts an empty block on the stack */
+void putBlock();
+/* removes the top block from the stack */
+void popBlock();
+/* inserts an identifier in the current block */
 void insertIdentifier(char *name, NodeType ntype, int etype, Node *paramlist);
+/* returns identifier type */
+NodeType lookupType(char *name);
+/* returns the evaltype of the identifier (type or returntype) */
+int lookupEvalType(char *name, NodeType ntype);
+/* return the paramaterlist of an function */
+Node* lookupParams(char *name);
 
 /* EOF symboltable.h*/
