c123c844c95f6163a071d999e431e2de3ce09429
CC007 <Coolcat_the_best2@hotmail.com>
2015-03-18 21:56:48 +0100

    finished tree.h struct and enum definitions

--- lab4/tree.h
+++ lab4/tree.h
@@ -3,17 +3,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "expressiontree.h"
+
 //enums
-typedef enum{
-    
+
+typedef enum {
+    decStmnt, assStmnt, funcCallStmnt, procCallStmnt, readCallStmnt, writeCallStmnt, ifStmnt, doStmnt
 } StmntKind;
 
+typedef enum{
+    constant, variable;
+} IDType;
+
+typedef enum{
+    stringPrint, expPrint
+} PrintKind;
 //ref defs
-typedef struct Statement *Stmnt;
 
 typedef struct FunctionDefinition *FuncDef;
 
 typedef struct ProcedureDefinition *ProcDef;
+
+typedef struct Statement *Stmnt;
+
+typedef struct Declaration *Dec;
 
 typedef struct Assignment *Ass;
 
@@ -21,15 +34,89 @@ typedef struct ReadCall *RCall;
 
 typedef struct WriteCall *WCall;
 
+typedef struct PrintableItem *Printable;
+
+// function call defined in expressiontree.h
+
 typedef struct ProcedureCall *ProcCall;
+
+typedef struct IfStatement *IfStmnt;
+
+typedef struct DoStatement *DoStmnt;
 
 
 //struct defs
-struct Statement{
+struct FunctionDefinition{
+    ID id;
+    int numParams;
+    ID *params;
+    int numStmnts;
+    Stmnt *stmnts;
+};
+
+struct ProcedureDefinition{
+    char *name;
+    int numParams;
+    ID *params;
+    int numStmnts;
+    Stmnt *stmnts;
+};
+
+struct Statement {
     StmntKind kind;
+
+    union {
+        Dec dec;
+        Ass assignment;
+        FuncCall funcCall;
+        ProcCall procCall;
+        RCall rCall;
+        WCall wCall;
+        IfStmnt ifStmnt;
+        DoStmnt doStmnt;
+    };
+};
+
+struct Declaration{
+    ID id;
+    IDType idType;
+    ExpTree expTree;
+};
+
+struct Assignment{
+    ID id;
+    ExpTree expTree;
+};
+
+struct ProcedureCall{
+    ID id;
+    int numParams;
+    Exp *params;
     
 };
 
+struct ReadCall{
+    ID *ids;
+};
+
+struct WriteCall{
+    Printable *items;
+};
+
+struct PrintableItem{
+    PrintKind kind;
+    
+    union{
+        char *string;
+        Exp exp; 
+    };
+};
+
+struct ProcedureCall {
+    char *name;
+    int numParams;
+    Exp *params;
+};
 
 // func list
 
