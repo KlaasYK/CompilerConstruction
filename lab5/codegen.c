fc05ee68ecc1a66303b22406e29a01b952d23b24
Klaas Kliffen <klaaskliffen@msn.com>
2015-04-09 20:34:52 +0200

    Skeleton for procedures and functions

--- lab5/codegen.c
+++ lab5/codegen.c
@@ -3,13 +3,33 @@
 
 #include "codegen.h"
 #include "tree.h"
+#include "symboltable.h"
 
 FILE *outputfile;
+
+int varcnt = 0;
+int lblcnt = 0;
 
 void WTF(char *code) {
 	fputs(code,outputfile);
 }
 
+char *getCTypeString(int type) {
+	switch(type) {
+		case BOOLEAN_TYPE:
+		case CONST_BOOLEAN_TYPE:
+			return "int";
+		case REF_BOOLEAN_TYPE:
+			return "int *";
+		case INTEGER_TYPE:
+		case CONST_INTEGER_TYPE:
+			return "Integer";
+		case REF_INTEGER_TYPE:
+			return "Integer *";
+		default:
+			return "void";
+	}
+}
 
 void writeHeaders() {
 		WTF("#include <stdio.h>\n");
@@ -18,6 +38,34 @@ void writeHeaders() {
 }
 
 
+void compileDo() {
+	
+}
+
+void compileIf() {
+	
+}
+
+void compileFunc(FuncDef function) {
+	WTF(getCTypeString(function->id->type));
+	WTF(" ");
+	WTF(function->id->name);
+	WTF("(");
+	// arguments
+	WTF(") {\n");
+	//body
+	WTF("}\n");
+}
+
+void compileProc(ProcDef procedure) {
+	WTF("void ");
+	WTF(procedure->name);
+	WTF("(");
+	// arguments
+	WTF(") {\n");
+	//body
+	WTF("}\n");
+}
 
 void compileMain() {
 	WTF("int main(int argc, char **argv){\n");
@@ -33,7 +81,15 @@ void generateCode(Prog program, char *outputfilename) {
 	
 	writeHeaders();
 	
-	//TODO: compile functions and procedures
+	// TODO: write constants and globals
+	
+	for (int i = 0; i < program->numProcDefs; i++) {
+		compileProc(program->procDefs[i]);
+	}
+	
+	for (int i = 0; i < program->numFuncDefs; i++) {
+		compileFunc(program->funcDefs[i]);
+	}
 	
 	compileMain();
 	
